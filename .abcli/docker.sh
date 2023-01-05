24122ae7dcc16052eb92a387a08052226e336743
kamangir <arash@kamangir.net>
2023-01-04 22:27:05 -0800

    validating the docker image - kamangir/bolt#742

---
+++ .abcli/docker.sh
@@ -0,0 +1,34 @@
+function sandbox_docker() {
+    local task=$(abcli_unpack_keyword $1 help)
+
+    if [ $task == "help" ] ; then
+        abcli_show_usage "sandbox docker build [start]" \
+            "build docker."
+        abcli_show_usage "sandbox docker start" \
+            "start docker."
+        return
+    fi
+
+    if [ "$task" == build ] ; then
+        local options=$2
+
+        local do_start=$(fast_option_int "$options" start 0)
+
+        pushd $abcli_path_git/sandbox > /dev/null
+        docker build -t kamangir/sandbox .
+        popd > /dev/null
+
+        if [ "$do_start" == 1 ] ; then
+            sandbox_docker start
+        fi
+    
+        return
+    fi
+
+    if [ "$task" == start ] ; then
+        docker run -it kamangir/sandbox
+        return
+    fi
+
+    abcli_log_error "-sandbox: docker: $task: command not found."
+}
