6637bccd3220911a5fe3659bf180e3f4f2b0a05c
kamangir <arash@kamangir.net>
2023-01-04 22:28:20 -0800

    validating the docker image - kamangir/bolt#742

--- .abcli/sandbox.sh
+++ .abcli/sandbox.sh
@@ -4,26 +4,13 @@ function sandbox() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ] ; then
-        abcli_show_usage "sandbox task_1" \
-            "run abct task_1."
-
-        if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
-            python3 -m sandbox --help
-        fi
-
+        sandbox_docker $@
         return
     fi
 
     local function_name=sandbox_$task
     if [[ $(type -t $function_name) == "function" ]] ; then
         $function_name ${@:2}
-        return
-    fi
-
-    if [ "$task" == "task_1" ] ; then
-        python3 -m sandbox \
-            task_1 \
-            ${@:2}
         return
     fi
 
